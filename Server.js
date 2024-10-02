3e7df84e55495c9aa8aa12989f864ec4b595ac59
Shruti08Deokule <shrutideokule5@gmail.com>
2024-10-02 21:15:19 +0530

    Add files via upload

--- Server.js
+++ Server.js
@@ -1,19 +1,26 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const app=express()
-const Port=(8000)
-app.use(express.json)
-const url="mongodb://localhost:27017/Fire"
-mongoose.connect(url).then(()=>{
-    console.log("connected")
-}) 
-const userSchema =new mongoose.Schema({
-    name:String,
-    age:Number
-})
-const userModel =new mongoose.Model("user",userSchema)
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+// After writing dotenv, we have to call its config file
+dotenv.config();
 
-app.post('/user',(req,res)=>{
-name:req.body.name,
-age:req.body.age
-})
+const userRouter=require("./routes/userRoute")
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log("Connected successfully");
+    app.listen(process.env.PORT || 3000, (err) => {
+      if (err) console.log(err);
+      console.log("Server running successfully at", process.env.PORT || 3000);
+    });
+  })
+  .catch((error) => {
+    console.log("Connection error:", error);
+  });
+
+app.use(userRouter)
